78118eacd983ca0ba3f041c5c38733636937788a
HappyJohn573 <78828034+HappyJohn573@users.noreply.github.com>
2021-02-12 00:22:45 -0600

    Update
    Code added to program.

--- src/driver/Dice.java
+++ src/driver/Dice.java
@@ -1,5 +1,19 @@
 package driver;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice
+{
+	int sides;
+	
+	Dice(int sides)
+	{
+		this.sides = sides;
+	}
+	
+	int roll()
+	{
+		Random r = new Random();
+		return r.nextInt(this.sides) + 1;
+	}
 }
